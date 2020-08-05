d4e6c94c01d15cad73ebed35fb8f1743bc298092
itsamanverma <amanvermame786@gmail.com>
2020-08-05 20:42:55 +0530

    add_category blade php design

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'parent_id','name', 'description', 'url', 'status' 
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+         'remember_token',
+    ];
+
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+
 }
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     //
+    public function addCategory(Request $request){
+        if ($request->isMethod('post')) {
+            # code...
+            $data = $request->all();
+            echo "<pre>"; print_r($data); die;
+        }
+        return view('admin.categories.add_category');
+    }
 }
