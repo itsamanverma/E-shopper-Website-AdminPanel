7ec6c3282b384b69f9743fd4aee3b08438a1a488
itsamanverma <amanvermame786@gmail.com>
2020-09-02 16:04:31 +0530

    add attributes

--- public/js/backend_js/matrix.form_validation.js
+++ public/js/backend_js/matrix.form_validation.js
@@ -221,4 +221,27 @@ $(document).ready(function () {
 		});
 	});
 
+	$(document).ready(function(){
+		var maxField = 10; //Input fields increment limitation
+		var addButton = $('.add_button'); //Add button selector
+		var wrapper = $('.field_wrapper'); //Input field wrapper
+		var fieldHTML = '<div class="field_wrapper" style="margin-left:180px;"><div><input type="text" name="sku[]" id="sku" placeholder="SKU" style="width:120px; margin-top:3px;"/> <input type="text" name="size[]" id="size" placeholder="Size" style="width:120px; margin-top:3px;"/> <input type="text" name="price[]" id="price" placeholder="Price" style="width:120px; margin-top:3px;"/> <input type="text" name="stock[]" id="stock" placeholder="Stock" style="width:120px; margin-top:3px;"/><a href="javascript:void(0);" class="remove_button"> Remove</a></div></div>'; //New input field html 
+		var x = 1; //Initial field counter is 1
+		
+		//Once add button is clicked
+		$(addButton).click(function(){
+			//Check maximum number of input fields
+			if(x < maxField){ 
+				x++; //Increment field counter
+				$(wrapper).append(fieldHTML); //Add field html
+			}
+		});
+		
+		//Once remove button is clicked
+		$(wrapper).on('click', '.remove_button', function(e){
+			e.preventDefault();
+			$(this).parent('div').remove(); //Remove field html
+			x--; //Decrement field counter
+		});
+	});
 });
--- resources/views/admin/products/add_attributes.blade.php
+++ resources/views/admin/products/add_attributes.blade.php
@@ -38,6 +38,18 @@
                 <label class="control-label">Product Color</label>
                 <label class="control-label"><strong>{{$productDetails->product_color}}</strong></label>
               </div>
+              <div class="control-group">
+                <label class="control-label"></label>
+                <div class="field_wrapper">
+                  <div>
+                      <input type="text" name="sku[]" id="sku" placeholder="SKU" style="width:120px;"/>
+                      <input type="text" name="size[]" id="size" placeholder="Size" style="width:120px;"/>
+                      <input type="text" name="price[]" id="price" placeholder="Price" style="width:120px;"/>
+                      <input type="text" name="stock[]" id="stock" placeholder="Stock" style="width:120px;"/>
+                      <a href="javascript:void(0);" class="add_button" title="Add field">Add</a>
+                  </div>
+                </div>
+              </div>
               <div class="form-actions">
                 <input type="submit" value="Add Attributes" class="btn btn-success">
               </div>
