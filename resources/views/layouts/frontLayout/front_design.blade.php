3adbf6a9962fb515f59950112c9c2e2d42fca871
itsamanverma <amanvermame786@gmail.com>
2020-09-03 23:42:49 +0530

    fonts url issue fixed

--- resources/views/layouts/frontLayout/front_design.blade.php
+++ resources/views/layouts/frontLayout/front_design.blade.php
@@ -17,7 +17,7 @@
     <script src="{{ asset('js/frontend_js/html5shiv.js') }}"></script>
     <script src="{{ asset('js/frontend_js/respond.min.js') }}"></script>
     <![endif]-->       
-    <link rel="shortcut icon" href="{{ asset('images/ico/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('images/ico/apple-touch-icon-144-precomposed.png') }}">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('images/ico/apple-touch-icon-114-precomposed.png') }}">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('images/ico/apple-touch-icon-72-precomposed.png') }}">
