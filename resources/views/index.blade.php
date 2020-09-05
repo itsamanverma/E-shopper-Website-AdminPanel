ce766b3a19859679533918cef0a9e0226bdb16f6
itsamanverma <amanvermame786@gmail.com>
2020-09-05 15:42:05 +0530

    design the E-shopper home & pass the dynamic products && price & product_name

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -217,14 +217,14 @@
                             <div class="single-products">
                                     <div class="productinfo text-center">
                                         <img src="{{ asset('images/frontend_images/home/product1.jpg') }}" alt="" />
-                                        <h2>$56</h2>
-                                        <p>Easy Polo Black Edition</p>
+                                        <h2>₹{{ $product->price }}</h2>
+                                        <p>{{ $product->product_name }}</p>
                                         <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                                     </div>
                                     <div class="product-overlay">
                                         <div class="overlay-content">
-                                            <h2>$56</h2>
-                                            <p>Easy Polo Black Edition</p>
+                                            <h2>₹{{ $product->price }}</h2>
+                                            <p>{{ $product->product_name }}</p>
                                             <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                                         </div>
                                     </div>
