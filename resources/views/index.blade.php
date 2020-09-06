daf1f53bcd7f90f3c440e4f91cca4bbf1c98b4cb
itsamanverma <amanvermame786@gmail.com>
2020-09-07 00:13:26 +0530

    show the categories && Sub-Categories in E-shopper home page side bar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -85,25 +85,7 @@
                         <div class="panel-group category-products" id="accordian">
                             <!--category-productsr-->
                             <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title">
-                                        <a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
-                                            <span class="badge pull-right"><i class="fa fa-plus"></i></span>
-                                            Sportswear
-                                        </a>
-                                    </h4>
-                                </div>
-                                <div id="sportswear" class="panel-collapse collapse">
-                                    <div class="panel-body">
-                                        <ul>
-                                            <li><a href="#">Nike </a></li>
-                                            <li><a href="#">Under Armour </a></li>
-                                            <li><a href="#">Adidas </a></li>
-                                            <li><a href="#">Puma</a></li>
-                                            <li><a href="#">ASICS </a></li>
-                                        </ul>
-                                    </div>
-                                </div>
+                                <?php echo $categories_menu; ?>
                             </div>
                             <div class="panel panel-default">
                                 <div class="panel-heading">
