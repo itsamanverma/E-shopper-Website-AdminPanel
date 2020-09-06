dbefc53366237a80b1b17d1d3f8094d914f0a9b1
itsamanverma <amanvermame786@gmail.com>
2020-09-06 17:28:02 +0530

    add the eloqoute qurey to random products

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -16,7 +16,14 @@ class IndexController extends Controller
      */
     public function index(){
         # code...
+        /* In Ascending order (by defualt) */
+        $productAll = Product::get();
+        /* In Descending order (by defualt) */
         $productAll = Product::orderby('id', 'DESC')->get();
+
+        /* In Random order */
+        $productAll = Product::inRandomOrder()->get();
+
         return view('index')->with(compact('productAll'));
     }
 }
