53bc28a7186b2b4e138844b67522f200c8a09040
itsamanverma <amanvermame786@gmail.com>
2020-09-08 12:21:26 +0530

    add sidebar category && sub-category

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -34,7 +34,7 @@ class IndexController extends Controller
             # code...
             $categories_menu .= "<div class='panel-heading'>
                                     <h4 class='panel-title'>
-                                        <a data-toggle='collapse' data-parent='#".$cat->id."' href='#".$cat->url."'>
+                                        <a data-toggle='collapse' data-parent='#accordian' href='#".$cat->id."'>
                                             <span class='badge pull-right'><i class='fa fa-plus'></i></span>
                                             ".$cat->name."
                                         </a>
@@ -45,7 +45,7 @@ class IndexController extends Controller
                                         <ul>";
                                         $sub_categories = Category::where(['parent_id' => $cat->id])->get();
                                             foreach ($sub_categories as $subcat) {
-                                                $categories_menu .= "<li><a href='#'>".$subcat->name." </a></li>";
+                                                $categories_menu .= "<li><a href='".$subcat->url."'>".$subcat->name." </a></li>";
                                             }
                                             $categories_menu .= "</ul>
                                     </div>
