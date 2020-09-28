791f4894ece7359bb1db54af2bf7895cd89f00c8
itsamanverma <amanvermame786@gmail.com>
2020-09-28 15:34:32 +0530

    trying the fixed the issue to show the main categoery type products

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,13 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public static function mainCategories()
+    {
+        # code...
+        $mainCategories = Category::where(['parent_id' => 0])->get();
+        $mainCategories = json_decode(json_encode($mainCategories));
+        // echo '<pre>'; print_r($mainCategories);
+        return $mainCategories;
+    }
 }
--- resources/views/layouts/frontLayout/front_header.blade.php
+++ resources/views/layouts/frontLayout/front_header.blade.php
@@ -1,3 +1,7 @@
+<?php
+use App\Http\Controllers\Controller;
+$mainCategories = Controller::mainCategories();
+?>
 <header id="header"><!--header-->
     <div class="header_top"><!--header_top-->
         <div class="container">
@@ -88,11 +92,9 @@
                             <li><a href="index.html" class="active">Home</a></li>
                             <li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
                                 <ul role="menu" class="sub-menu">
-                                    <li><a href="shop.html">Products</a></li>
-                                    <li><a href="product-details.html">Product Details</a></li> 
-                                    <li><a href="checkout.html">Checkout</a></li> 
-                                    <li><a href="cart.html">Cart</a></li> 
-                                    <li><a href="login.html">Login</a></li> 
+                                    @foreach ($mainCategories as $cat)
+                                    <li><a href="{{asset('products/'.$cat->url)}}">{{$cat->name}}</a></li>
+                                    @endforeach
                                 </ul>
                             </li> 
                             <li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
