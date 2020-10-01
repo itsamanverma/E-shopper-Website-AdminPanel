75742393efd057daf763a440312df9a5f49018e3
itsamanverma <amanvermame786@gmail.com>
2020-10-01 22:51:18 +0530

    change listing page

--- resources/views/products/listing.blade.php
+++ resources/views/products/listing.blade.php
@@ -88,25 +88,25 @@
                             <div class="panel panel-default">
                                 @foreach ($categories as $cat)
                                     @if ($cat->status == "1")
-                                    <div class="panel-heading">
-                                        <h4 class="panel-title">
-                                            <a data-toggle="collapse" data-parent="#accordian" href="#{{ $cat->id }}">
-                                                <span class="badge pull-right"><i class="fa fa-plus"></i></span>
-                                                {{$cat->name }}
-                                            </a>
-                                        </h4>
-                                    </div>
-                                    <div id="{{ $cat->id }}" class="panel-collapse collapse">
-                                        <div class="panel-body">
-                                            <ul>
-                                                @foreach ($cat->categories as $subcat)
-                                                    @if ($subcat->status == "1")
-                                                        <li><a href="{{ asset('/products/'.$subcat->url)}}">{{$subcat->name }}</a></li>
-                                                    @endif
-                                                @endforeach
-                                            </ul>
+                                        <div class="panel-heading">
+                                            <h4 class="panel-title">
+                                                <a data-toggle="collapse" data-parent="#accordian" href="#{{ $cat->id }}">
+                                                    <span class="badge pull-right"><i class="fa fa-plus"></i></span>
+                                                    {{$cat->name }}
+                                                </a>
+                                            </h4>
                                         </div>
-                                    </div>
+                                        <div id="{{ $cat->id }}" class="panel-collapse collapse">
+                                            <div class="panel-body">
+                                                <ul>
+                                                    @foreach ($cat->categories as $subcat)
+                                                        @if ($subcat->status == "1")
+                                                            <li><a href="{{ asset('/products/'.$subcat->url)}}">{{$subcat->name }}</a></li>
+                                                        @endif
+                                                    @endforeach
+                                                </ul>
+                                            </div>
+                                        </div>
                                     @endif
                                 @endforeach
                             </div>
